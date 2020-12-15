3292edd9b1e2afbe51f1f5a0bbd3fa1408c1ea98
Stanmina <70628632+Stanmina@users.noreply.github.com>
2020-12-15 08:52:11 +0100

    Add project files.

---
+++ WindowsFormsApp4/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿
+namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbDagenVanDeWeek = new System.Windows.Forms.ComboBox();
+            this.btnWhile = new System.Windows.Forms.Button();
+            this.btnDoWhile = new System.Windows.Forms.Button();
+            this.btnFor = new System.Windows.Forms.Button();
+            this.tbTafel = new System.Windows.Forms.TextBox();
+            this.tbTafelVan = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // cmbDagenVanDeWeek
+            // 
+            this.cmbDagenVanDeWeek.FormattingEnabled = true;
+            this.cmbDagenVanDeWeek.Location = new System.Drawing.Point(48, 49);
+            this.cmbDagenVanDeWeek.Name = "cmbDagenVanDeWeek";
+            this.cmbDagenVanDeWeek.Size = new System.Drawing.Size(228, 21);
+            this.cmbDagenVanDeWeek.TabIndex = 0;
+            this.cmbDagenVanDeWeek.SelectedIndexChanged += new System.EventHandler(this.cmbDagenVanDeWeek_SelectedIndexChanged);
+            // 
+            // btnWhile
+            // 
+            this.btnWhile.Location = new System.Drawing.Point(48, 91);
+            this.btnWhile.Name = "btnWhile";
+            this.btnWhile.Size = new System.Drawing.Size(75, 23);
+            this.btnWhile.TabIndex = 1;
+            this.btnWhile.Text = "While loop";
+            this.btnWhile.UseVisualStyleBackColor = true;
+            this.btnWhile.Click += new System.EventHandler(this.btnWhile_Click);
+            // 
+            // btnDoWhile
+            // 
+            this.btnDoWhile.Location = new System.Drawing.Point(48, 120);
+            this.btnDoWhile.Name = "btnDoWhile";
+            this.btnDoWhile.Size = new System.Drawing.Size(75, 23);
+            this.btnDoWhile.TabIndex = 2;
+            this.btnDoWhile.Text = "Do While";
+            this.btnDoWhile.UseVisualStyleBackColor = true;
+            this.btnDoWhile.Click += new System.EventHandler(this.btnDoWhile_Click);
+            // 
+            // btnFor
+            // 
+            this.btnFor.Location = new System.Drawing.Point(48, 149);
+            this.btnFor.Name = "btnFor";
+            this.btnFor.Size = new System.Drawing.Size(75, 23);
+            this.btnFor.TabIndex = 3;
+            this.btnFor.Text = "For loop";
+            this.btnFor.UseVisualStyleBackColor = true;
+            this.btnFor.Click += new System.EventHandler(this.btnFor_Click);
+            // 
+            // tbTafel
+            // 
+            this.tbTafel.Location = new System.Drawing.Point(129, 91);
+            this.tbTafel.Multiline = true;
+            this.tbTafel.Name = "tbTafel";
+            this.tbTafel.Size = new System.Drawing.Size(147, 139);
+            this.tbTafel.TabIndex = 4;
+            // 
+            // tbTafelVan
+            // 
+            this.tbTafelVan.Location = new System.Drawing.Point(48, 178);
+            this.tbTafelVan.Name = "tbTafelVan";
+            this.tbTafelVan.Size = new System.Drawing.Size(75, 20);
+            this.tbTafelVan.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(332, 365);
+            this.Controls.Add(this.tbTafelVan);
+            this.Controls.Add(this.tbTafel);
+            this.Controls.Add(this.btnFor);
+            this.Controls.Add(this.btnDoWhile);
+            this.Controls.Add(this.btnWhile);
+            this.Controls.Add(this.cmbDagenVanDeWeek);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbDagenVanDeWeek;
+        private System.Windows.Forms.Button btnWhile;
+        private System.Windows.Forms.Button btnDoWhile;
+        private System.Windows.Forms.Button btnFor;
+        private System.Windows.Forms.TextBox tbTafel;
+        private System.Windows.Forms.TextBox tbTafelVan;
+    }
+}
+
---
+++ WindowsFormsApp4/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        enum dagenVanDeWeek
+        {
+            maandag,
+            dinsdag,
+            woensdag,
+            donderdag,
+            vrijdag,
+            zaterdag,
+            zondag
+        }
+
+        bool formLoaded = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            cmbDagenVanDeWeek.DataSource = Enum.GetNames(typeof(dagenVanDeWeek));
+        }
+
+        private void cmbDagenVanDeWeek_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (formLoaded)
+            {
+                string dag = cmbDagenVanDeWeek.SelectedItem.ToString();
+                string text = "Het is vandaag ";
+
+                switch (dag)
+                {
+                    case "maandag":
+                        text += dag;
+                        MessageBox.Show(text);
+                        break;
+                    case "dinsdag":
+                        text += dag;
+                        MessageBox.Show(text);
+                        break;
+                    case "woensdag":
+                        text += dag;
+                        MessageBox.Show(text);
+                        break;
+                    case "donderdag":
+                        text += dag;
+                        MessageBox.Show(text);
+                        break;
+                    case "vrijdag":
+                        text += dag;
+                        MessageBox.Show(text);
+                        break;
+                    case "zaterdag":
+                        text += dag;
+                        MessageBox.Show(text);
+                        break;
+                    case "zondag":
+                        text += dag;
+                        MessageBox.Show(text);
+                        break;
+                    default:
+                        MessageBox.Show("Error");
+                        break;
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            formLoaded = true;
+        }
+
+        private void btnWhile_Click(object sender, EventArgs e)
+        {
+            Random objRandom = new Random();
+            int getal = objRandom.Next(1, 11);
+
+            while(getal < 5)
+            {
+                getal = objRandom.Next(1, 11);
+            }
+
+            MessageBox.Show(getal.ToString());
+        }
+
+        private void btnDoWhile_Click(object sender, EventArgs e)
+        {
+            int getal = 1;
+            Random objRandom = new Random();
+
+            do
+            {
+                getal = objRandom.Next(1, 11);
+            } while (getal < 5);
+            MessageBox.Show(getal.ToString());
+        }
+
+        private void btnFor_Click(object sender, EventArgs e)
+        {
+            int tafelVan;
+
+            if (int.TryParse(tbTafelVan.Text, out tafelVan))
+            {
+                if (tafelVan > 0 && tafelVan < 11)
+                {
+                    int product;
+                    string tafel = "";
+                    for (int i = 1; i < 11; i++)
+                    {
+                        product = i * tafelVan;
+
+                        tafel += i + " x " + tafelVan + " = " + product + "\r\n";
+                    }
+
+                tbTafel.Text = tafel;
+                }
+                else
+                {
+                    MessageBox.Show("Getal moet tussen 0 en 10 zijn");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Getal moet tussen 0 en 10 zijn");
+            }
+        }
+    }
+}
